b1e4c1177eb64ee10de33a39e2a40b1c42a74c69
Jeerhz <adlesalem@hotmail.fr>
2025-04-23 21:03:05 +0800

    add RLS and select data policies in proejcts sql script.

--- docker/volumes/db/init/102-projects.sql
+++ docker/volumes/db/init/102-projects.sql
@@ -4,29 +4,28 @@
 CREATE TABLE IF NOT EXISTS public.projects (
   id                UUID               PRIMARY KEY
                           DEFAULT gen_random_uuid(),
-
   created_at        TIMESTAMPTZ        NOT NULL
                           DEFAULT now(),
-
   institution       public.institution NOT NULL,
-
   country           TEXT,
-
   sector            public.sector,
-
   status            public.status,
-
   project_name      TEXT,
-
   project_url       TEXT,
-
   project_id        TEXT,
-
   commitment_amount NUMERIC,
-
   approval_date     TIMESTAMPTZ,
-
   metadata          JSONB,
-
   region            public.region
 );
+
+-- 2) Enable RLS
+ALTER TABLE IF EXISTS public.profiles
+  ENABLE ROW LEVEL SECURITY;
+
+-- 3) Policies
+DROP POLICY IF EXISTS profiles_select ON public.profiles;
+CREATE POLICY profiles_select
+  ON public.profiles
+  FOR SELECT
+  USING (true);
